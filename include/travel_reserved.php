8b3466a74088c643cac9cf442fb5435ba47bc508
Yooo31 <mrts.yoan@gmail.com>
2022-06-27 10:53:52 +0200

    Ajout de la requete qui change le nombre de place en bd lorsqu'un voyage est réservé

--- include/travel_reserved.php
+++ include/travel_reserved.php
@@ -5,3 +5,41 @@
     <a href="/" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Retourner à l'accueil</a>
   </div>
 </div>
+
+<?php
+  $servername='localhost';
+  $username='agence';
+  $password='Voyage31.';
+  $dbname = "alltravel";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sqlplace = "SELECT * FROM AllTravel WHERE id = '$actual_id_travel'";
+
+  $resultsqlplace = $conn->query($sqlplace);
+
+  if ($resultsqlplace->num_rows > 0) {
+    // output data of each row
+    while($row = $resultsqlplace->fetch_assoc()) {
+      $before_total_place = intval($row["numberplace"]);
+    }
+  } else {
+    echo "0 results";
+  }
+
+  $after_total_place = $before_total_place - $NumberReserved;
+
+  $change_number = "UPDATE AllTravel SET numberplace = $after_total_place WHERE id = '$actual_id_travel';";
+
+  if ($conn->query($change_number) === True) {
+
+  } else {
+    echo "Error updating record: " . $conn->error;
+  }
+
+  $conn->close();
